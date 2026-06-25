edcaac877542049c229b5981e5ff554712b75e4a
Abhijith-TG <abhijithtg62@gmail.com>
2026-06-25 20:09:46 +0530

    feat: Updated the responsiveness of topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,16 +5,16 @@ import { MdKeyboardArrowDown } from 'react-icons/md'
 function TopBar() {
 
   return (
-    <div className=' flex ps-6 pe-2 py-2 text-white bg-purple-900/40 backdrop-blur-xl rounded-full justify-between max-w-200 min-w-150  items-center'>
+    <div className=' flex ps-6 pe-2 py-2 text-white bg-purple-900/40 backdrop-blur-xl rounded-full justify-between max-w-200 min-w-80 sm:min-w-150  items-center transition-all'>
         <div className='text-xl'>Dashboard</div> 
         <div className='flex items-center gap-3'>
-            <div className='p-2.5 bg-purple-900/60 rounded-full' > <BsQuestionCircle size={18}/> </div>
-            <div className='p-2.5 bg-purple-900/60 rounded-full'> <IoNotificationsOutline size={18} /> </div>
-            <div className='flex items-center gap-3 bg-purple-900/60 px-2.5 py-1.5 rounded-full justify-between '>
+            <div className='  p-1.5  sm:p-2.5 bg-purple-900/60 rounded-full' > <BsQuestionCircle size={18}/> </div>
+            <div className='p-1.5  sm:p-2.5 bg-purple-900/60 rounded-full'> <IoNotificationsOutline size={18} /> </div>
+            <div className='flex items-center gap-1 sm:gap-3 bg-purple-900/60 px-1.5  sm:px-2.5 py-1.5 rounded-full justify-between transition-all'>
                 <div>
                     <img className='w-7 h-7 rounded-full' src="https://aniyuki.com/wp-content/uploads/2025/02/aniyuki-solo-leveling-gif-11.gif" alt="" />
                 </div>
-                < p className='text-sm'>{name}</p>
+                < p className='text-sm hidden sm:flex transition-all'>{name}</p>
                 <MdKeyboardArrowDown/>
             </div>
             
