2d365765f8ab386782fe6d7f0e9c3c3409bf6503
Abhijith-TG <abhijithtg62@gmail.com>
2026-07-09 18:20:50 +0530

    feat: updated the overview 2 responsiveness

--- src/components/Overview2.tsx
+++ src/components/Overview2.tsx
@@ -9,9 +9,9 @@ import XpChart from "./XpChart";
 function OverviewMain({activity}:activityProps) {
   return (
     <div className="px-5 py-7 bg-purple-900/40 w-fit rounded-xl backdrop-blur-xl text-white flex flex-wrap">
-        <div className="flex justify-between flex-wrap w-full">
-            <div className="flex flex-col gap-3">
-                <h1 className="text-xl">Activities Overview</h1>
+        <div className="flex justify-between  w-full gap-4">
+            <div className="flex flex-col gap-2">
+                <h1 className=" text-lg sm:text-xl">Activities Overview</h1>
                 <p className="text-xs text-gray-300">Overview of the activities and tasks completed.</p>
             </div>
             <div>
