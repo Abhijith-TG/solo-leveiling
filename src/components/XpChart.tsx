fb25aa7da72018b243a6af8aecfeea78e3b7da50
Abhijith-TG <abhijithtg62@gmail.com>
2026-07-09 19:17:21 +0530

    feat: Updated the xp size for overview 2

--- src/components/XpChart.tsx
+++ src/components/XpChart.tsx
@@ -18,12 +18,12 @@ function XpChart({ activity }: activityProps) {
           {activity.slice(-7).map((a, i) => (
             <div key={i} className="flex flex-col items-center">
               <div
-                className={`sm:w-10 w-5 bg-green-400/80 rounded-t-lg`}
+                className={`sm:w-10 w-5   bg-green-400/80 rounded-t-lg`}
                 style={{
                   height: `${(a.xp / 315) * 180}px`,
                 }}
               ></div>
-              <p className="text-xs">{a.xp} xp</p>
+              <p className="text-[9px] sm:text-xs">{a.xp} xp</p>
             </div>
           ))}
         </div>
