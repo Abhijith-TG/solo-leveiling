f38017dd18b46b1e8229f8a11416cb06547258be
Abhijith-TG <abhijithtg62@gmail.com>
2026-07-09 19:37:00 +0530

    feat: Updated the dashboard name size

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,7 @@ function TopBar() {
 
   return (
     <div className=' flex ps-5 pe-2 py-2 text-white bg-purple-900/40 backdrop-blur-xl rounded-full justify-between max-w-200 min-w-70 sm:min-w-150 lg:min-w-full items-center transition-all'>
-        <div className='text-xl'>Dashboard</div> 
+        <div className=' text-xl sm:text-2xl'>Dashboard</div> 
         <div className='flex items-center gap-3'>
             {/* <div className='  p-1.5  sm:p-2.5 bg-purple-900/60 rounded-full' > <BsQuestionCircle size={18}/> </div> */}
             <div className='p-1.5  sm:p-2.5 bg-purple-900/60 rounded-full'> <IoNotificationsOutline size={18} /> </div>
